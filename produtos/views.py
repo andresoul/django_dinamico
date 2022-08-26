2b0168c46a5e574aaee05615145bdbfa12e1e4fa
André Jesus <andresoul@gmail.com>
2022-08-26 17:02:46 -0300

    Initial commit

---
+++ produtos/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def produtos(request):
+    return render(request, 'produtos/index.html')
+
+def teclados(request):
+    return render(request, 'produtos/teclados.html')
+
+def mouses(request):
+    return render(request, 'produtos/mouses.html')
+
+def headsets(request):
+    return render(request, 'produtos/headsets.html')
+
